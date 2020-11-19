e76af8b9afc9f86c316abf8ac1f7bf85b419ee75
Paul Standley <paulstandley1972@gmail.com>
2020-11-19 19:15:56 +0000

    overloading operators

--- Lesson9CCP.cpp
+++ Lesson9CCP.cpp
@@ -1,20 +1,19 @@
 #include "pch.h"
 #include <iostream>
+#include "Lesson9Header.h"
 
+void overloading_arithmetic_operators_using_friend_functions()
+{
 
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    overloading_arithmetic_operators_using_friend_functions();
+    operator_overloading();
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ Lesson9Header.h
@@ -0,0 +1,9 @@
+#ifndef LESSON9CPP
+#define LESSON9CPP
+
+void operator_overloading();
+void overloading_arithmetic_operators_using_friend_functions();
+
+
+#endif // !LESSON9CPP
+
