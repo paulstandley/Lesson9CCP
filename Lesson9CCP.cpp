071420370a29ed389bd4b77e82ce0fc03adcd0b0
Paul Standley <paulstandley1972@gmail.com>
2020-12-05 13:07:10 +0000

    lesson 9.2 quiz d

--- Lesson9CCP.cpp
+++ Lesson9CCP.cpp
@@ -201,6 +201,23 @@ void lesson92quiz()
     f6.print();
 
     std::cout << "------------------" << '\n';
+
+    /*d) If we remove the const from the Fraction * Fraction operator, 
+    the following line from the main function no longer works. Why?
+
+    The non-const multiplication operator looks like this
+
+    Fraction operator*(Fraction &f1, Fraction &f2)
+
+    This doesn't work anymore
+
+    Fraction f6{ Fraction{1, 2} * Fraction{2, 3} * Fraction{3, 4} };
+
+    We’re multiplying temporary Fraction objects,
+    but non-const references cannot bind to temporaries.
+    
+    */
+
 }
 
 int main()
